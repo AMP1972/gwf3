40c8df5a9d4616716ca96c7a13d7ff771741e20b
Andre Michael Pietroschek <58571653+AMP1972@users.noreply.github.com>
2019-12-05 21:48:37 +0100

    Update Cyberpunk.php

--- core/module/Dog/dog_modules/dog_module/Shadowlamb/city/Redmond/npc/Cyberpunk.php
+++ core/module/Dog/dog_modules/dog_module/Shadowlamb/city/Redmond/npc/Cyberpunk.php
@@ -8,17 +8,18 @@ final class Redmond_Cyberpunk extends SR_NPC
 	{
 		return array(
 			'weapon' => array('BaseballBat', 'IronPipe', 'Club', 'Stiletto', 'WoodNunchaku'),
-			'armor' => 'Clothes',
-			'legs' => 'Trousers',
-			'boots' => array('Sneakers', 'Sandals'),
+			'armor' => 'LeatherVest',
+			'legs' => 'Shorts',
+			'boots' => array('Sneakers', 'Boots'),
 		);
 	}
 	public function getNPCModifiers() {
 		return array(
 			'nuyen' => rand(20, 30),
-			'base_hp' => rand(0, 2),
+			'base_hp' => rand(0, 3),
+			'melee' => rand(0, 4),
 			'strength' => rand(2, 3),
-			'quickness' => rand(2, 3),
+			'quickness' => rand(2, 6),
 			'distance' => rand(0, 2),
 		);
 	}
