428a97192d27a80d0c198253930b14c9b9e79540
Andre Michael Pietroschek <58571653+AMP1972@users.noreply.github.com>
2019-12-05 21:57:36 +0100

    Update ToughGuy.php

--- core/module/Dog/dog_modules/dog_module/Shadowlamb/city/Redmond/npc/ToughGuy.php
+++ core/module/Dog/dog_modules/dog_module/Shadowlamb/city/Redmond/npc/ToughGuy.php
@@ -1,7 +1,7 @@
 <?php
 final class Redmond_ToughGuy extends SR_NPC
 {
-	public function getNPCLevel() { return 3; }
+	public function getNPCLevel() { return 4; }
 	public function getNPCPlayerName() { return 'Tough Guy'; }
 	public function getNPCMeetPercent(SR_Party $party) { return 50.00; }
 	public function canNPCMeet(SR_Party $party) { return true; }
@@ -10,17 +10,18 @@ final class Redmond_ToughGuy extends SR_NPC
 	{
 		return array(
 			'weapon' => array('BaseballBat', 'IronPipe'),
-			'armor' => 'Clothes',
+			'armor' => 'LeatherVest',
 			'legs' => 'Trousers',
-			'boots' => array('Sneakers', 'Sandals'),
+			'boots' => array('Sneakers', 'ElvenBoots'),
 		);
 	}
 	public function getNPCInventory() { return array(); }
 	public function getNPCModifiers() {
 		return array(
 			'nuyen' => rand(30, 40),
-			'base_hp' => rand(-2, 0),
+			'base_hp' => rand(-1, 1),
 			'distance' => rand(0, 2),
+			'melee' => rand(0, 5),
 		);
 	}
 	
