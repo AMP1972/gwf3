804f81611389ab242a8645cfe1b656a791e5ccce
Andre Michael Pietroschek <58571653+AMP1972@users.noreply.github.com>
2019-12-07 07:01:30 +0100

    Update Stiletto.php

--- core/module/Dog/dog_modules/dog_module/Shadowlamb/item/meleeweapon/Stiletto.php
+++ core/module/Dog/dog_modules/dog_module/Shadowlamb/item/meleeweapon/Stiletto.php
@@ -1,19 +1,18 @@
 <?php
 final class Item_Stiletto extends SR_MeleeWeapon
 {
-	public function getAttackTime() { return 40; }
+	public function getAttackTime() { return 29; }
 	public function getItemLevel() { return 2; }
-	public function getItemWeight() { return 350; }
-	public function getItemPrice() { return 175; }
+	public function getItemWeight() { return 280; }
+	public function getItemPrice() { return 125; }
 	public function getItemRange() { return 0.9; }
-	public function getItemDescription() { return 'A surgical instrument. Actually not made for combat.'; }
+	public function getItemDescription() { return 'A slim, cheap blade preferred by punks & criminals. Actually not made for combat.'; }
 	public function getItemModifiersA(SR_Player $player)
 	{
 		return array(
-			'attack' => 2.1, 
+			'attack' => 2.0, 
 			'min_dmg' => 2.0,
 			'max_dmg' => 6.5,
 		);
 	}
 }
-?>
